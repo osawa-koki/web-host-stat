4ec5d869fc98c42763425f99af89e337e72960b5
osawa-koki <github@osawa.dev>
2022-12-29 00:10:03 +0900

    静的ファイルに対応させた。

--- app/app.go
+++ app/app.go
@@ -1,18 +1,21 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Start() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello go gin 👍",
-		})
-	})
+
+	r.StaticFile("/", "./web/index.html")
+	r.StaticFile("/index", "./web/index.html")
+	r.StaticFile("/about", "./web/about.html")
+	r.Static("/_next", "./web/_next")
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run("0.0.0.0:80") // 0.0.0.0:8080 でサーバーを立てます。
+	r.Run("0.0.0.0:80") // 0.0.0.0:80 でサーバーを立てます。
 }
